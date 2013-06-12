b4e0ba676823be8a1fb04a60fec26ec6544f5aee
shaynarang <shaynarang@gmail.com>
2013-06-12 18:40:19 -0500

    added cucumber testing for user log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 NSSYearbook::Application.routes.draw do
+
+  root :to => "home#index"
+
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
